fc8d422873eedb9c0ce3160b4155c04332a9f469
jasad <maurice_jaso@hotmail.co.uk>
2018-02-16 18:42:21 +0100

    Added a gitignore file. Updated the app.py file to include routing. Added the templates.

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__)
 
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/all')
+def all():
+    return render_template('all.html')
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/remove')
+def remove():
+    return render_template('remove.html')
+
+if __name__ == "__main__":
+    app.run()

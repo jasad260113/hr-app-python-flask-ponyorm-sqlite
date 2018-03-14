d7fc8d155cc3eb0aaeb416718ad087ca845bdf31
jasad <maurice_jaso@hotmail.co.uk>
2018-03-14 15:31:49 +0100

    Implemented delete employee.

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 from pony.orm import *
 #from wtforms import Form, BooleanField, StringField, PasswordField, validators
 import sqlite3
+import json, logging
+
+logging.getLogger().setLevel(logging.INFO)
+
 app = Flask(__name__)
 
 db = Database()
@@ -26,84 +30,72 @@ def main():
     return render_template('index.html')
 
 @app.route('/all')
-@db_session # When interacting with the db from an app with pony orm then the code needs to be wrapped in a @db_session() decorator
+@db_session
 def all():
-
     all_employees = select (e for e in Employees)[:]
-
-    # # Open connection to database
-    # connection = sqlite3.connect('hrapp.db')
-    # cursor = connection.cursor()
-    #
-    # cursor.execute('select * from employees')
-    #
-    # data = cursor.fetchall()
-    # list = data
-    #
-    # # Close connection to database
-    # cursor.close()
-    # connection.close()
-
-    #return render_template('all.html', list=list)
     return render_template('all.html', all_employees=all_employees)
 
 @app.route('/register', methods=['POST'])
 @db_session
 def register():
-
-    firstname = request.form['firstName']
-    lastname = request.form['lastName']
-    address = request.form['address']
-    address2 = request.form['address2']
-    city = request.form['city']
-    province = request.form['province']
-    postcode = request.form['postcode']
-    gender = request.form['gender']
-    phone = request.form['phone']
-    # print 'firstname: ' + firstname
-
-    new_employee = Employees(firstname=firstname,
-                             lastname=lastname,
-                             address=address,
-                             address2=address2,
-                             city=city,
-                             province=province,
-                             postcode=postcode,
-                             gender=gender,
-                             phone=phone)
+    new_employee = Employees(
+        firstname = request.form['firstName'],
+        lastname = request.form['lastName'],
+        address = request.form['address'],
+        address2 = request.form['address2'],
+        city = request.form['city'],
+        province = request.form['province'],
+        postcode = request.form['postcode'],
+        gender = request.form['gender'],
+        phone = request.form['phone']
+    )
     commit()
-
-    # # Open connection to database
-    # connection = sqlite3.connect('hrapp.db')
-    # cursor = connection.cursor()
-    #
-    # # Insert data into database
-    # cursor.execute('INSERT INTO employees (firstname, lastname, address, address2, city, province, postcode, gender, phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    # (firstname, lastname, address, address2, city, province, postcode, gender, phone))
-    #
-    # # Commit data into database
-    # connection.commit()
-    #
-    # # Close connection to database
-    # cursor.close()
-    # connection.close()
-
     return render_template('submit.html')
 
 @app.route('/add')
 def add():
     return render_template('add.html')
 
-@app.route('/remove')
-def remove():
-    return render_template('remove.html')
+@app.route('/update', methods=['POST'])
+@db_session
+def update():
+    # Get the employee object from the db
+    employee = Employees[request.form['id']]
+    # Store values from form in employee object
+    employee.firstname = request.form['firstName']
+    employee.lastname = request.form['lastName']
+    employee.address = request.form['address']
+    employee.address2 = request.form['address2']
+    employee.city = request.form['city']
+    employee.province = request.form['province']
+    employee.postcode = request.form['postcode']
+    employee.gender = request.form['gender']
+    employee.phone = request.form['phone']
+    commit()
+    return render_template('submit.html')
 
-@app.route('/employee')
-def employee():
-    # Open database connection and cursor
-    # SELECT employee WHERE id passed in is EQUAL TO id in database
-    return render_template('employee.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
+@app.route('/postmethod', methods = ['POST'])
+@db_session
+def get_post_json_data():
+    js_data = request.json
+    logging.info('js_data: ')
+    logging.info(js_data)
+    logging.info(type(js_data))
+    for key in js_data:
+        logging.info('ID: ' + js_data[key])
+        # Employees[js_data[key]].delete()
+    return 'get_post_json_data was successful'
+
+@app.route('/employee/<int:id>')
+@db_session
+def employee(id):
+    list = select (e for e in Employees if e.id == id)[:]
+    # record = Employees[id]
+    return render_template('employee.html', list=list)
 
 if __name__ == "__main__":
     app.run(debug=True)
